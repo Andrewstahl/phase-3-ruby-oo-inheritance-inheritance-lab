d4b6a732a853d73fe8cea90850448500396eadc1
Andrew Stahl <andrewstahl96@gmail.com>
2022-09-01 20:24:47 -0400

    Files working with ruby

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,22 @@
-class Student 
+require 'pry'
+
+require_relative './user.rb'
+
+class Student < User
+  attr_accessor :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(subject)
+    @knowledge << subject
+  end
+
+  def knowledge
+    @knowledge
+  end
+
+  # binding.pry
 
 end
